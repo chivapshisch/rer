8c3b50832d6a72c646d41888bbb6d8760e366854
Ilia <iluhinila@gmail.com>
2024-09-07 17:20:32 +0300

    <Обновленная база данных>

--- index/admin.py
+++ index/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import * 
 
+
 admin.site.register(Cars)
+admin.site.register(Types)
+admin.site.register(Status)
+admin.site.register(Gorod)
--- index/models.py
+++ index/models.py
@@ -1,6 +1,47 @@
 from django.db import models
 
+
+class Types(models.Model):
+    typer = models.CharField(max_length = 200)
+
+    def __str__(self):
+        return self.typer
+    class Meta:
+        verbose_name = 'Модель автомобиля'
+        verbose_name_plural = 'Модель автомобиля'
+
+class Status(models.Model):
+    polozhenie = models.CharField(max_length = 8)
+    def __str__(self):
+        return self.polozhenie
+    
+    class Meta:
+        verbose_name = 'Статус'
+        verbose_name_plural = 'Статус'
+
+class Gorod(models.Model):
+    city = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.city
+    
+    class Meta:
+        verbose_name = 'Города'
+        verbose_name_plural = 'Города'
+
 class Cars(models.Model):
+    town = models.ForeignKey(Gorod, on_delete= models.CASCADE)
     number = models.CharField(max_length = 6)
-    model = models.CharField(max_length = 50)
+    model = models.ForeignKey(Types, on_delete= models.CASCADE)
     price = models.IntegerField()
+    status = models.ForeignKey(Status, on_delete=models.CASCADE)
+    cvet = models.CharField(max_length=150)
+    god_vypuska = models.IntegerField()
+
+    def __str__(self):
+        return self.number
+    
+    class Meta:
+        verbose_name = 'Машины'
+        verbose_name_plural = 'Машины'
+

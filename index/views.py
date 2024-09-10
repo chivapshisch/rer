2eadff1e9d826872480c3652af46cdbbf9596a57
Ilia <iluhinila@gmail.com>
2024-09-10 22:27:37 +0300

    Попытка добавить поиск

--- index/views.py
+++ index/views.py
@@ -26,6 +26,28 @@ class NewCar(generics.CreateAPIView):
     queryset = Cars.objects.all()
     serializer_class = CarsSerializer
 
-class Change(generics.UpdateAPIView):
-    queryset = Cars.objects.all()
-    serializer_class = CarsSerializer
+class Change(APIView):
+    def try_putter(self, pk):
+        try:
+            return Cars.objects.get(pk=pk)
+        except Cars.DoesNotExist:
+            raise Http404
+    
+    def put(self, request, pk):
+        snip = self.try_putter(pk)
+
+   
+        ser = CarsSerializer(snip)
+        data = {
+            'town': ser.data['town'],
+            'number': ser.data['number'],
+            'model': ser.data['model'],
+            'price': request.data['price'],
+            'status': request.data['status'],
+            'cvet': ser.data['cvet'],
+            'god_vypuska': ser.data['god_vypuska']
+        }
+        serial_end = CarsSerializer(data = data)
+        if serial_end.is_valid():
+            serial_end.save()
+            return Response(serial_end.data)
